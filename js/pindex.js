d7e55fe716fde877969df67da0c07a03a15fc941
necromman <necromman@gmail.com>
2017-08-20 17:43:23 +0900

    first commit

---
+++ js/pindex.js
@@ -0,0 +1,10 @@
+
+setTimeout(function start (){
+    $('.bar').each(function(i){
+        var $bar = $(this);
+        setTimeout(function(){
+            $bar.css('width', $bar.attr('data-percent'));
+        }, i*50);
+    });
+}, 100)
+
