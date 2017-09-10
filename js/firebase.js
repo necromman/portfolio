e2dad380cc150164471f8a54d9e6eba9dd826d12
lee <necromman@gmail.com>
2017-09-10 18:30:10 +0900

    UPDATE

--- js/firebase.js
+++ js/firebase.js
@@ -1,70 +1,82 @@
-    $(function() {
-        var auth = firebase.auth();
-        var database = firebase.database();
-        var userInfo;
-        auth.onAuthStateChanged(function(user) {
-            if (user) {
-                userInfo = user;
-                get_text_list()
-            } else {
-                firebase.auth().signInAnonymously(); // 익명로그인
-            }
-        });
-        var Now = new Date();
-        var NowTime = Now.getFullYear();
-        NowTime += '-' + (Now.getMonth()+1);
-        NowTime += '-' + Now.getDate();
-        NowTime += ' ' + Now.getHours();
-        NowTime += ':' + Now.getMinutes();
-        NowTime += ':' + Now.getSeconds();
-
-        var psearch = $("#p-search");
-        $("input[name=p-search]").keydown(function(key) {
-            if (key.keyCode == 13) { //키가 13이면 실행 (엔터는 13)
-                goSearch();
-                $("#pac-input").val("");
-            }
-        });
-        psearch.click(function() {
-            goSearch();
-        });
-
-        function goSearch() {
-            var keyword = $("#pac-input").val();
-            if (!keyword == "") {
-                // =========== 파이어베이스 입력 시작
-                var publicKeyIn = database.ref('/채팅/' + "퍼블릭채팅");
-                publicKeyIn.push({
-                    keyword: keyword,
-                    date: NowTime
-                });
-                // =========== 파이어베이스 입력 끝
-            } else {
-                alert("입력값이 없어요")
-            }
-        }
-
-
-        function get_text_list() {
-            var publicGetKeyIn = firebase.database().ref('/채팅/' + "퍼블릭채팅");
-            publicGetKeyIn.on('child_added', public_on_child_added);
-        }
-
-        function public_on_child_added(data) {
-            // var key = data.key;
-            var sData = data.val();
-            var Kword = sData.keyword;
-            var Kdate = sData.date;
-
-            var html2 =
-                "<div class='timeline-item'>"+
-                "<div class='timeline-icon'>"+ Kdate + "</div>"+
-                "<div class='timeline-content'>"+
-                "<h2>익명</h2>"+
-                "<p>"+ Kword + "</p>"+
-                "</div>"+
-                "</div>";
-
-            $("#timeline").append(html2);
+$(function () {
+    var auth = firebase.auth();
+    var database = firebase.database();
+    var userInfo;
+    auth.onAuthStateChanged(function (user) {
+        if (user) {
+            userInfo = user;
+            get_text_list()
+        } else {
+            firebase.auth().signInAnonymously(); // 익명로그인
         }
     });
+    var Now = new Date();
+    var NowTime = Now.getFullYear();
+    NowTime += '-' + leadingZeros((Now.getMonth() + 1),2);
+    NowTime += '-' + leadingZeros(Now.getDate(),2);
+    NowTime += ' ' + leadingZeros(Now.getHours(),2);
+    NowTime += ':' + leadingZeros(Now.getMinutes(),2);
+    NowTime += ':' + leadingZeros(Now.getSeconds(),2);
+
+    function leadingZeros(n, digits) {
+        var zero = '';
+        n = n.toString();
+
+        if (n.length < digits) {
+            for (var i = 0; i < digits - n.length; i++)
+                zero += '0';
+        }
+        return zero + n;
+    }
+
+
+    var psearch = $("#p-search");
+    $("input[name=p-search]").keydown(function (key) {
+        if (key.keyCode == 13) { //키가 13이면 실행 (엔터는 13)
+            goSearch();
+            $("#pac-input").val("");
+        }
+    });
+    psearch.click(function () {
+        goSearch();
+    });
+
+    function goSearch() {
+        var keyword = $("#pac-input").val();
+        if (!keyword == "") {
+            // =========== 파이어베이스 입력 시작
+            var publicKeyIn = database.ref('/채팅/' + "퍼블릭채팅");
+            publicKeyIn.push({
+                keyword: keyword,
+                date: NowTime
+            });
+            // =========== 파이어베이스 입력 끝
+        } else {
+            alert("입력값이 없어요")
+        }
+    }
+
+
+    function get_text_list() {
+        var publicGetKeyIn = firebase.database().ref('/채팅/' + "퍼블릭채팅");
+        publicGetKeyIn.on('child_added', public_on_child_added);
+    }
+
+    function public_on_child_added(data) {
+        // var key = data.key;
+        var sData = data.val();
+        var Kword = sData.keyword;
+        var Kdate = sData.date;
+
+        var html2 =
+            "<div class='timeline-item'>" +
+            "<div class='timeline-icon'>" + Kdate + "</div>" +
+            "<div class='timeline-content'>" +
+            "<h2>익명</h2>" +
+            "<p>" + Kword + "</p>" +
+            "</div>" +
+            "</div>";
+
+        $("#timeline").prepend(html2);
+    }
+});
--- js/pindex.js
+++ js/pindex.js
@@ -1,24 +1,24 @@
-
-setTimeout(function start (){
-    $('.bar').each(function(i){
+setTimeout(function start() {
+    $('.bar').each(function (i) {
         var $bar = $(this);
-        setTimeout(function(){
+        setTimeout(function () {
             $bar.css('width', $bar.attr('data-percent'));
-        }, i*1);
+        }, i * 1);
     });
 }, 5);
 
 $(function () {
-    $(".label").click(function () {
+
+    $(".label a").click(function () {
         var _this = $(this);
-        _this.parent().next(".expand").stop().slideToggle();
-        if(_this.hasClass("open")){
-            _this.removeClass("open");
-            _this.addClass("close")
-        }else if(_this.hasClass("close")){
-            _this.removeClass("close");
-            _this.addClass("open")
+        _this.parent().parent().next(".expand").stop().slideToggle();
+        if (_this.parent().hasClass("open")) {
+            _this.parent().removeClass("open");
+            _this.parent().addClass("close")
+        } else if (_this.parent().hasClass("close")) {
+            _this.parent().removeClass("close");
+            _this.parent().addClass("open")
         }
-    })
+    });
 
 });
